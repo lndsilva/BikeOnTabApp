58258517abd071bcdbd3c26102dea78c838360b6
lndsilva <lndsilva@hotmail.com>
2025-08-22 11:05:31 -0300

    janela de menu principal

--- app/src/main/java/br/com/local/bikeontabapp/ItensBikeActivity.java
+++ app/src/main/java/br/com/local/bikeontabapp/ItensBikeActivity.java
@@ -1,9 +1,13 @@
 package br.com.local.bikeontabapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +18,10 @@ import com.google.android.material.tabs.TabLayout;
 public class ItensBikeActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
+
+    AdapterTabItens adapterTabItens;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +34,45 @@ public class ItensBikeActivity extends AppCompatActivity {
             return insets;
         });
 
+        toolbar = findViewById(R.id.tooBarItensBike);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MenuPrincipalActivity.class));
+            }
+        });
+
         tabLayout = findViewById(R.id.tabLayoutItensBike);
         viewPager2 = findViewById(R.id.viewPagerItensBike);
 
+        adapterTabItens = new AdapterTabItens(this);
 
+        viewPager2.setAdapter(adapterTabItens);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 }
