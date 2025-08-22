b851545258848ad1c3135c9a1458db71626a5437
lndsilva <lndsilva@hotmail.com>
2025-08-22 11:55:04 -0300

    menu bottombar

--- app/src/main/java/br/com/local/bikeontabapp/LojaFragment.java
+++ app/src/main/java/br/com/local/bikeontabapp/LojaFragment.java
@@ -2,21 +2,42 @@ package br.com.local.bikeontabapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class LojaFragment extends Fragment {
 
+public class LojaFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_loja, container, false);
 
+        bottomNavigationView = view.findViewById(R.id.bottomNavViewLoja);
+
+        if (savedInstanceState == null){
+
+
+        }
+
+
+
 
         return view;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
